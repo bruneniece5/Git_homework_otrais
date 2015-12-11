09c5bc0f7f09bb3c2e26fe20d5e5a8e775b170b2
Laura <bruneniece5@inbox.lv>
2015-12-11 10:41:17 +0200

    Vienkarss commit

---
+++ Git_3/Git_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Git_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Hello world!\n Press any key to exit!");
+            Console.ReadKey();
+
+        }
+    }
+}
