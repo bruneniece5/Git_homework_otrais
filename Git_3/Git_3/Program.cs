48a6cb53d647d9f6e60f785d34ea823c4df76217
Laura <bruneniece5@inbox.lv>
2015-12-11 10:56:06 +0200

    Pievieno mainigos x,y

--- Git_3/Git_3/Program.cs
+++ Git_3/Git_3/Program.cs
@@ -10,6 +10,28 @@ namespace Git_3
         static void Main(string[] args)
         {
             Console.WriteLine("\n Hello world!\n Press any key to exit!");
+            int x, y, c = 0;
+
+            for (int i = 0; i == c; i++)
+            {
+                try
+                {
+                    Console.Write("\n Ievadiet vienu skaitli:  ");
+                    x = Int32.Parse(Console.ReadLine());
+
+                    Console.Write(" Ievadiet otru skaitli: ");
+                    y = Int32.Parse(Console.ReadLine());
+
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine("\n " + error.Message);
+                    c++;
+                }
+            }
+
+            Console.ReadKey();
+
             Console.ReadKey();
 
         }
